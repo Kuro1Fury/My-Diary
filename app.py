e4e4b63603e15a8a25726726f5463f4d95b2bc2b
Allan Ji <75246208+Kuro1Fury@users.noreply.github.com>
2022-12-29 17:24:19 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ from pymongo import MongoClient
 
 
 # def create_app():
-app = Flask(__name__)
+#     app = Flask(__name__)
 #     client = MongoClient(
 #         "mongodb+srv://Kuro1_Fury:skycoder135790@my-diary-application.tibejqh.mongodb.net/test")
 #     app.db = client.MyDiary
 
-@app.route("/", methods=["GET", "POST"])
-def home():
-    if request.method == "POST":
-        entry_content = request.form.get("contentEntry")
-        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+#     @app.route("/", methods=["GET", "POST"])
+#     def home():
+#         if request.method == "POST":
+#             entry_content = request.form.get("contentEntry")
+#             formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
 #             app.db.entries.insert_one(
 #                 {"content": entry_content, "date": formatted_date})
 
@@ -25,7 +25,38 @@ def home():
 #              )
 #             for entry in app.db.entries.find({})
 #         ]
-#     return render_template("home.html", entries=entries_with_date)
-    return "Hello?"
+#         return render_template("home.html", entries=entries_with_date)
+
+#     return app
+
+entries = []
+
+# def create_app():
+app = Flask(__name__)
+#     client = MongoClient(
+#         "mongodb+srv://Kuro1_Fury:skycoder135790@my-diary-application.tibejqh.mongodb.net/test")
+#     app.db = client.MyDiary
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        entry_content = request.form.get("contentEntry")
+        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+
+        entries.append((entry_content, formatted_date))
+#             app.db.entries.insert_one(
+#                 {"content": entry_content, "date": formatted_date})
+
+    entries_with_date = [
+        (entry[0],
+            entry[1],
+            datetime.datetime.strptime(
+            entry[1], "%Y-%m-%d").strftime("%b %d")
+         )
+        for entry in entries
+    ]
+    return render_template("home.html", entries=entries_with_date)
+    # return "Hello?"
 
 #     return app
